3775e8720d4040bce01d3ff54438a33babef7b04
JasonCRACK666 <emerzonkolki@gmail.com>
2022-05-25 09:22:36 -0500

    Reducción de código en el evento del menu y submenu

--- js/subMenu.js
+++ js/subMenu.js
@@ -2,11 +2,20 @@ const menuSubNav = document.getElementById("subMenu");
 const subNav = document.getElementById("subNav");
 
 menuSubNav.addEventListener("click", () => {
-  if (subNav.style.display === "none") {
-    subNav.style.display = "flex";
-    menuSubNav.innerHTML = `<i class="fa-solid fa-sort-up"></i>`;
-    menuSubNav.style.backgroundColor = "#006241";
-  } else if (subNav.style.display === "flex") {
+  // if (subNav.style.display === "none") {
+  //   subNav.style.display = "flex";
+  //   menuSubNav.innerHTML = `<i class="fa-solid fa-sort-up"></i>`;
+  //   menuSubNav.style.backgroundColor = "#006241";
+  // } else if (subNav.style.display === "flex") {
+  //   subNav.style.display = "none";
+  //   menuSubNav.innerHTML = `<i class="fa-solid fa-caret-down"></i>`;
+  //   menuSubNav.style.backgroundColor = "#009c63";
+  // } else {
+  //   subNav.style.display = "flex";
+  //   menuSubNav.innerHTML = `<i class="fa-solid fa-sort-up"></i>`;
+  //   menuSubNav.style.backgroundColor = "#006241";
+  // }
+  if (subNav.style.display === "flex") {
     subNav.style.display = "none";
     menuSubNav.innerHTML = `<i class="fa-solid fa-caret-down"></i>`;
     menuSubNav.style.backgroundColor = "#009c63";
