2ae26fb639f43b26413e7211066a3383ed03f959
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2022-10-28 22:47:28 -0500

    logic auth in navbar

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,56 @@
-const NavbarMenu = document.getElementById("menu");
-const navbar = document.querySelector(".navbar__links");
+const menu = document.getElementById("menu");
 
-NavbarMenu.addEventListener("click", () => {
-  // if (navbar.style.display === "none") {
-  //   navbar.style.display = "flex";
-  // } else if (navbar.style.display === "flex") {
-  //   navbar.style.display = "none";
-  // } else {
-  //   navbar.style.display = "flex";
-  // }
+const toggleMenu = () => {
+  const navbar = document.querySelector(".navbar__links")
   if (navbar.style.display === "flex") {
-    navbar.style.display = "none";
+    navbar.style.display = "none"
   } else {
-    navbar.style.display = "flex";
+    navbar.style.display = "flex"
   }
-});
+}
+
+menu.addEventListener("click", toggleMenu)
+
+const handleLogout = () => {
+  window.localStorage.removeItem('isAuth')
+  window.location.href = 'index.html'
+}
+
+const createOptionsNotAuth = container => {
+  let linkLogin = document.createElement('a')
+  linkLogin.innerText = 'Iniciar sesión'
+  linkLogin.setAttribute('href', 'authLogin.html')
+  linkLogin.classList.add('navbar__auth--link')
+  linkLogin.classList.add('navbar__auth--login')
+  container.appendChild(linkLogin)
+
+  let linkRegister = document.createElement('a')
+  linkRegister.innerText = 'Registrarse'
+  linkRegister.setAttribute('href', 'authRegister.html')
+  linkRegister.classList.add('navbar__auth--link')
+  linkRegister.classList.add('navbar__auth--register')
+  container.appendChild(linkRegister)
+}
+
+const createOptionsIsAuth = container => {
+  let btnLogout = document.createElement('button')
+  btnLogout.innerText = 'Cerrar sesión'
+  btnLogout.classList.add('navbar__auth--link')
+  btnLogout.classList.add('navbar__auth--logout')
+  btnLogout.addEventListener('click', handleLogout)
+  container.appendChild(btnLogout)
+}
+
+const setOptionsAuth = () => {
+  let container = document.querySelector('.navbar__auth')
+  const isAuth = window.localStorage.getItem('isAuth')
+
+  if (!isAuth) {
+    createOptionsNotAuth(container)
+    return
+  }
+
+  createOptionsIsAuth(container)
+}
+
+setOptionsAuth()
