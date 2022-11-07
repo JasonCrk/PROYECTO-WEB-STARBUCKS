e05c8960c059ed3d3450fc6930eda6c6089c57f1
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2022-11-06 22:53:40 -0500

    auth options for mobile

--- js/menu.js
+++ js/menu.js
@@ -2,21 +2,17 @@ const menu = document.getElementById("menu");
 
 const toggleMenu = () => {
   const navbar = document.querySelector(".navbar__links")
-  if (navbar.style.display === "flex") {
-    navbar.style.display = "none"
-  } else {
-    navbar.style.display = "flex"
-  }
+  navbar.style.display = navbar.style.display === 'flex' ? 'none' : 'flex'
 }
-
-menu.addEventListener("click", toggleMenu)
 
 const handleLogout = () => {
   window.localStorage.removeItem('isAuth')
-  window.location.href = 'index.html'
+  window.location.replace('index.html')
 }
 
 const createOptionsNotAuth = container => {
+  let navbarLinks = document.querySelector('.navbar__links')
+
   let linkLogin = document.createElement('a')
   linkLogin.innerText = 'Iniciar sesión'
   linkLogin.setAttribute('href', 'authLogin.html')
@@ -30,15 +26,50 @@ const createOptionsNotAuth = container => {
   linkRegister.classList.add('navbar__auth--link')
   linkRegister.classList.add('navbar__auth--register')
   container.appendChild(linkRegister)
+
+  let linkLoginResponsive = document.createElement('a')
+  linkLoginResponsive.innerText = 'Iniciar sesión'
+  linkLoginResponsive.setAttribute('href', 'authLogin.html')
+  linkLoginResponsive.classList.add('navbar__link')
+
+  let listItemLogin = document.createElement('li')
+  listItemLogin.classList.add('auth-activate')
+  listItemLogin.appendChild(linkLoginResponsive)
+
+  let linkRegisterResponsive = document.createElement('a')
+  linkRegisterResponsive.innerText = 'Registrarse'
+  linkRegisterResponsive.setAttribute('href', 'authRegister.html')
+  linkRegisterResponsive.classList.add('navbar__link')
+
+  let listItemRegister = document.createElement('li')
+  listItemRegister.classList.add('auth-activate')
+  listItemRegister.appendChild(linkRegisterResponsive)
+
+  navbarLinks.appendChild(listItemLogin)
+  navbarLinks.appendChild(listItemRegister)
 }
 
 const createOptionsIsAuth = container => {
+  let navbarLinks = document.querySelector('.navbar__links')
+
   let btnLogout = document.createElement('button')
   btnLogout.innerText = 'Cerrar sesión'
   btnLogout.classList.add('navbar__auth--link')
   btnLogout.classList.add('navbar__auth--logout')
   btnLogout.addEventListener('click', handleLogout)
   container.appendChild(btnLogout)
+
+  let linkLogoutResponsive = document.createElement('a')
+  linkLogoutResponsive.innerText = 'Cerrar sesión'
+  linkLogoutResponsive.classList.add('navbar__link')
+  linkLogoutResponsive.classList.add('navbar__link-logout')
+  linkLogoutResponsive.addEventListener('click', handleLogout)
+
+  let listItemLogout = document.createElement('li')
+  listItemLogout.classList.add('logout-activate')
+  listItemLogout.appendChild(linkLogoutResponsive)
+
+  navbarLinks.appendChild(listItemLogout)
 }
 
 const setOptionsAuth = () => {
@@ -53,4 +84,5 @@ const setOptionsAuth = () => {
   createOptionsIsAuth(container)
 }
 
+menu.addEventListener("click", toggleMenu)
 setOptionsAuth()
