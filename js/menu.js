5171a7e3e7031bc8c76fb428c149b10d32aa5094
EPPR27 <113651234+EPPR27@users.noreply.github.com>
2022-11-12 14:23:40 -0500

    carrusel
    Agrego esto

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,7 @@
 const menu = document.getElementById("menu");
+const desayuno = ['../images/DESAYUNO_PARA_DOS_V4.png','../images/desayuno1.png','../images/desayuno2.png','../images/desayuno3.png','../images/desayuno4.png','../images/desayuno5.png','../images/desayuno6.png']
+const antojo = ['../images/PACK_ANTOJOS_V2.png','../images/antojo1.png','../images/antojo2.png','../images/antojo3.png','../images/antojo4.png','../images/antojo5.png','../images/antojo6.png']
+const box = ['../images/SWEET_BOX_V3.png','../images/box1.png','../images/box2.png','../images/box3.png','../images/box4.png','../images/box5.png','../images/box6.png']
 
 const toggleMenu = () => {
   const navbar = document.querySelector(".navbar__links")
@@ -8,6 +11,41 @@ const toggleMenu = () => {
     navbar.style.display = "flex"
   }
 }
+var i=0
+var g=0
+var f=0
+function carruselDesayuno(e){
+  document.getElementsByClassName('pack__contenedor-desayuno-imagen')[0].setAttribute('src',desayuno[i])
+  i++
+  if(i>=desayuno.length)i=0
+  ciclo= setTimeout(carruselDesayuno,1000)
+}
+function carruselAntojo(e){
+  document.getElementsByClassName('pack__contenedor-desayuno-imagen')[1].setAttribute('src',antojo[g])
+  g++
+  if(g>=desayuno.length)g=0
+  ciclo= setTimeout(carruselAntojo,1000)
+}
+function carruselBox(e){
+  document.getElementsByClassName('pack__contenedor-desayuno-imagen')[2].setAttribute('src',box[f])
+  f++
+  if(i>=desayuno.length)f=0
+  ciclo= setTimeout(carruselBox,1000)
+}
+
+function original(e){
+  clearTimeout(ciclo)
+document.getElementsByClassName('pack__contenedor-desayuno-imagen')[0].setAttribute('src','../images/DESAYUNO_PARA_DOS_V4.png')
+document.getElementsByClassName('pack__contenedor-desayuno-imagen')[1].setAttribute('src','../images/PACK_ANTOJOS_V2.png')
+document.getElementsByClassName('pack__contenedor-desayuno-imagen')[2].setAttribute('src','../images/SWEET_BOX_V3.png')
+}
+document.getElementsByClassName('pack__contenedor-desayuno-imagen')[0].setAttribute('onmouseover','carruselDesayuno(this)')
+document.getElementsByClassName('pack__contenedor-desayuno-imagen')[1].setAttribute('onmouseover','carruselAntojo(this)')
+document.getElementsByClassName('pack__contenedor-desayuno-imagen')[2].setAttribute('onmouseover','carruselBox(this)')
+document.querySelectorAll('.pack__contenedor-desayuno-imagen').forEach(x =>{
+  x.setAttribute('onmouseout','original(this)')
+})
+
 
 menu.addEventListener("click", toggleMenu)
 
