2d184d7568133b24ce427b70b124bdceea8aa265
EPPR27 <113651234+EPPR27@users.noreply.github.com>
2022-11-26 09:00:13 -0500

    Index y js
    correcciones en index images para evitar repeticiones y carrusel completo

--- js/carruselFavoritos.js
+++ js/carruselFavoritos.js
@@ -1,6 +1,6 @@
-const Duodesayunos = ['../images/DESAYUNO_PARA_DOS_V4.png', '../images/desayuno1.png', '../images/desayuno2.png', '../images/desayuno3.png', '../images/desayuno4.png', '../images/desayuno5.png', '../images/desayuno6.png']
-const DesayunosXdos = ['../images/PACK_ANTOJOS_V2.png', '../images/antojo1.png', '../images/antojo2.png', '../images/antojo3.png', '../images/antojo4.png', '../images/antojo5.png', '../images/antojo6.png']
-const PacksAntojos = ['../images/SWEET_BOX_V3.png', '../images/box1.png', '../images/box2.png', '../images/box3.png', '../images/box4.png', '../images/box5.png', '../images/box6.png']
+const Duodesayunos = ['../images/DESAYUNO_PARA_DOS_V4.png','../images/desayunoDuo1.png', '../images/desayunoDuo2.png']
+const DesayunosXdos = ['../images/desayunoDuo3.png','../images/desayunoDuo4.png','../images/PACK_ANTOJOS_V2.png']
+const PacksAntojos = ['../images/SWEET_BOX_V3.png', '../images/antojoDuo1.png','../images/antojoDuo2.png','../images/antojoDuo3.png']
 
 let DuodesIndice = 0
 let DesXdosIndice = 0
